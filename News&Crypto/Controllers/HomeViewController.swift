b26732e8f50bd0bd06f9bf8343c9f73f903dc98f
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2022-10-06 23:18:56 +0300

    start init HomeViewController

--- News&Crypto/Controllers/HomeViewController.swift
+++ News&Crypto/Controllers/HomeViewController.swift
@@ -1,13 +1,6 @@
-//
-//  HomeViewController.swift
-//  News&Crypto
-//
-//  Created by Игорь Тимофеев on 23.09.22.
-//
-
 import UIKit
 
-class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +9,5 @@ class HomeViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
